17eae3eec3cabd0187a76160f8ead74db750e122
Dom B <dominicbrown4@hotmail.com>
2023-11-13 22:16:34 +0000

    adding up-to-date files with corrected CSS code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Daily-Planner-App
 A repo to hold the code for the Daily Planner Application
+
+
+
+
+
+# Resources
+
+- DayJS for setting current date - 
+https://day.js.org/docs/en/display/format
+
+- DayJS for getting current hour - 
+https://day.js.org/docs/en/get-set/hour
+
+- Highlighting current time table row -
+https://stackoverflow.com/questions/69963399/higlight-table-row-based-on-current-time
+
+- Colour coding time blocks - 
+https://stackoverflow.com/questions/62904905/how-do-i-color-code-time-blocks-using-js
+
+- Save button help
+https://stackoverflow.com/questions/10333230/save-input-data-to-localstorage-on-button-click
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,35 @@ var currentDate = dayjs().format('dddd, MMM DD');
 // Setting content of currentDay to date
 
 document.getElementById('currentDay').textContent = currentDate;
+
+// Getting current hour 
+
+var currentHour = dayjs().format('H');
+
+// adding classes depending on time 
+
+document.querySelectorAll('.time-block').forEach(function(block) {
+    var blockHour = parseInt(block.id);
+    if (blockHour < currentHour) {
+        block.classList.remove('present');
+        block.classList.remove('future');
+        block.classList.add('past');
+    } else if (blockHour == currentHour) {
+        block.classList.remove('past');
+        block.classList.remove('future');
+        block.classList.add('present');
+    } else {
+        block.classList.remove('past');
+        block.classList.remove('present');
+        block.classList.add('future');
+    }
+});
+
+// Code to fix error with hour styling
+
+varHourElement = block.querySelector('.hour');
+if (hourElement) {
+    hourElement.classList.add('hour');
+}
+
+// Code for save button
